1cac8a1cc414c4486c086c4eb8b6966be725f871
Andrei Shabalin <andreishabalindev@outlook.com>
2020-10-30 20:19:34 +0300

    HTML5 create hedear and add image

--- index.php
+++ index.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <!-- ------------------------------------------------------------------- -->
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- WebSite Icon -->
+        <link rel="shortcut icon" href="./src/icon/book.png" type="image/x-icon">
+    <!-- Style link -->
+        <link rel="stylesheet" href="./style/Reset.css">
+        <link rel="stylesheet" href="./style/BaseStyle.css">
+    <!-- ------------------------------------------------------------------- -->
+        <title>Главная | BOOK.COM </title>
+    <!-- ------------------------------------------------------------------- -->
 </head>
 <body>
     <header>
         <!-- Header -->
-
+        <figure class="logo">
+            <!-- Logo img -->
+            <a href="">
+              <img src="./src/icon/book.png" alt="logo">  
+            </a>
+        </figure>
+        <h1 class="title_web">
+            <!-- Title site -->
+            BOOK.COM
+        </h1>
+        <div class="block_auto">
+            <!--  -->
+            <a href="#">Войти</a>
+            <a href="#">Зарегистрироваться</a>
+        </div>
     </header>
     <main>
         <section>
