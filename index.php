9a67b21479b8ee9d41edfc35e29f79b7d22e37cb
Andrei Shabalin <andreishabalindev@outlook.com>
2020-10-30 19:55:04 +0300

    Созданна логическое разделения документ

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <!-- Header -->
+
+    </header>
+    <main>
+        <section>
+            <!-- Slider -->
+            
+        </section>
+        <section>
+            <!-- Inform -->
+
+        </section>
+        <section>
+            <!-- Galery -->
+
+        </section>
+        <section>
+            <!-- Form -->
+
+        </section>
+    </main>
+    <footer>
+        <!-- Footer -->
+
+    </footer>
+</body>
+</html>
